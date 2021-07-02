4920ecc2d6be7ba066f77a0dc6ad34bd638a8ae5
mudit <mud.khare07@gmail.com>
2021-07-03 00:33:22 +0530

    part 3

---
+++ client/src/actions/auth.js
@@ -0,0 +1,24 @@
+import { AUTH} from '../constants/actionTypes';
+import * as api from '../api/index.js';
+
+export const signin = (formData, history) => async (dispatch) => {
+    try {
+        
+
+        history.pushState()
+    } catch (error) {
+        console.log(error);
+    }
+
+};
+
+export const signup = (formData, history) => async (dispatch) => {
+    try {
+        
+
+        history.pushState()
+    } catch (error) {
+        console.log(error);
+
+    }
+};
